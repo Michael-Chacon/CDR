3dbbcbd1f7be389c04b19d82ed3251e89aa5bb69
Michael-Chacon <michaelchacon02@gmail.com>
2022-07-27 15:23:59 -0500

    Agrugué los campos para auditar las acciones en la DB

--- database.sql
+++ database.sql
@@ -1,136 +1,7 @@
 CREATE DATABASE cdr_beta;
 USE cdr_beta;
-
-CREATE TABLE grado(
-		id INT(4) AUTO_INCREMENT NOT NULL,
-		nombre_g VARCHAR(3) NOT NULL,
-		CONSTRAINT pk_grado PRIMARY KEY(id)
-)ENGINE=InnoDb;
-
-CREATE TABLE materias_base(
-	id_base INT(4) AUTO_INCREMENT NOT NULL,
-	id_area_m INT(4) NOT NULL,
-	nombre_materia VARCHAR(50) NOT NULL,
-	porcentaje_materia_b INT(3) NOT NULL,
-	icono VARCHAR(60) NOT NULL,
-	CONSTRAINT pk_materias_base PRIMARY KEY (id_base),
-	CONSTRAINT fk_base_area FOREIGN KEY (id_area_m) REFERENCES areas(id_area) ON DELETE CASCADE
-)ENGINE=InnoDb;
-
-CREATE TABLE materia(
-		id INT(4) AUTO_INCREMENT NOT NULL,
-		id_grado_mat INT(4) NOT NULL,
-		id_materia_area INT(4) NOT NULL,
-		nombre_mat VARCHAR(30) NOT NULL,
-		indicadores_mat TEXT NOT NULL,
-		porcentaje_materia INT(3) NOT NULL,
-		icono VARCHAR(30)  NOT NULL,
-		asignacion VARCHAR(2) NOT NULL,
-		CONSTRAINT pk_materias PRIMARY KEY(id),
-		CONSTRAINT fk_grado_materia FOREIGN KEY(id_grado_mat) REFERENCES grado(id) ON DELETE CASCADE,
-		CONSTRAINT fk_materia_area FOREIGN KEY (id_materia_area) REFERENCES areas (id_area) ON DELETE CASCADE
-)ENGINE=InnoDb;
-
-CREATE TABLE periodo(
-	id INT(4) NOT NULL,
-	nombre_periodo VARCHAR(10) NOT NULL,
-	fecha_inicio DATE NOT NULL,
-	fecha_fin DATE NOT NULL,
-	CONSTRAINT pk_periodo PRIMARY KEY(id)
-)ENGINE=InnoDb;
-
-INSERT INTO periodo VALUES (1, '2022-01-01', '2022-02-02');
-INSERT INTO periodo VALUES (2, '2022-03-03', '2022-04-04');
-INSERT INTO periodo VALUES (3, '2022-05-05', '2022-06-06');
--- -----------------------------------------------------
-
-CREATE TABLE padres(
-	id INT(4) AUTO_INCREMENT NOT NULL,
-	nombre_m VARCHAR(25) NULL,
-	apellidos_m VARCHAR(30) NULL,
-	fecha_nacimiento_m DATE NULL,
-	edad_m INT(2) NULL,
-	tipo_identificacion_m VARCHAR(11) NULL,
-	numero_m INT(12) NULL,
-	lugar_expedicion_m VARCHAR(50) NULL, 
-	fecha_expedicion_m DATE NULL,
-	telefono_m BIGINT(10) NULL,
-	ocupacion_m VARCHAR(50) NULL,
-	nombre_p VARCHAR(25) NULL,
-	apellidos_p VARCHAR(30) NULL,
-	fecha_nacimiento_p DATE NULL,
-	edad_p INT(2) NULL,
-	tipo_identificacion_p VARCHAR(11) NULL,
-	numero_p INT(12) NULL,
-	lugar_expedicion_p VARCHAR(50) NULL, 
-	fecha_expedicion_p DATE NULL,
-	telefono_p BIGINT(10) NULL,
-	ocupacion_p VARCHAR(50) NULL,
-	direccion VARCHAR(30) NOT NULL,
-	correo VARCHAR(30) NOT NULL, 
-	CONSTRAINT pk_padres PRIMARY KEY(id)
-)ENGINE=InnoDb;
-
-CREATE TABLE estudiante(
-	id INT(4) AUTO_INCREMENT NOT NULL,
-	id_gradoE INT(4) NOT NULL,
-	id_familia_e INT(4) NOT NULL,
-	nombre_e VARCHAR(20) NOT NULL,
-	pellidos_e VARCHAR(20) NOT NULL,
-	fecha_nacimiento_e  DATE NOT NULL,
-	edad_e 	INT(2) NOT NULL,
-	sexo_e VARCHAR(10) NOT NULL,
-	tipo_identificacion_e VARCHAR(11)  NOT NULL,
-	numero_e INT(11) NOT NULL,
-	lugar_expedicion_e VARCHAR(50) NOT NULL,
-	fecha_expedicion_e DATE NOT NULL,
-	direccion_e VARCHAR(30) NOT NULL,
-	telefono_e BIGINT(10) NULL,
-	correo_e VARCHAR(50) NULL,
-	religion_e VARCHAR(20) NOT NULL,
-	incapacidad_medica_e VARCHAR(255) NOT NULL,
-	grupo_sanguineo_e VARCHAR(2) NOT NULL, 
-	rh_e VARCHAR(1) NOT NULL,
-	transporte VARCHAR(2) NOT NULL,
-	pae VARCHAR(2) NOT NULL,
-	img 	VARCHAR(255) NULL,
-	CONSTRAINT pk_estudiante PRIMARY KEY(id),
-	CONSTRAINT fk_grado_estudiante FOREIGN KEY(id_gradoE) REFERENCES grado(id),
-	CONSTRAINT fk_padres_estudiante FOREIGN KEY(id_familia_e) REFERENCES padres(id) ON DELETE CASCADE
-)ENGINE=InnoDb;
-
-CREATE TABLE docente(
-	id INT(4) AUTO_INCREMENT NOT NULL,
-	nombre_d VARCHAR(20) NOT NULL,
-	apellidos_d VARCHAR(20) NOT NULL,
-	fecha_nacimiento_d DATE NOT NULL,
-	edad_d INT(2) NOT NULL,
-	sexo_d VARCHAR(10) NOT NULL,
-	tipo_identificacion_d VARCHAR(10) NOT NULL,
-	numero_d INT(10) NOT NULL,
-	lugar_expedicion_d VARCHAR(50) NOT NULL,
-	fecha_expedicion_d DATE NOT NULL,
-	direccion_d VARCHAR(30) NOT NULL,
-	telefono_d BIGINT(10) NOT NULL,
-	correo_d VARCHAR(50) NOT NULL,
-	religion_d VARCHAR(20) NOT NULL,
-	incapacidad_medica_d VARCHAR(255) NOT NULL,
-	grupo_sanguineo_d VARCHAR(2) NOT NULL,
-	rh_d VARCHAR(1) NOT NULL,
-	fecha_posesion_d DATE NOT NULL,
-	numero_acta_posesion_d BIGINT NOT NULL,
-	numero_resolucion_posesion_d BIGINT NOT NULL,
-	pregrado_d VARCHAR(2) NOT NULL,
-	nombre_pregrado_d VARCHAR(30) NOT NULL,
-	posgrado_d VARCHAR(2) NOT NULL,
-	nombre_posgrado_d VARCHAR(30) NOT NULL,
-	img 	VARCHAR(255) NULL,
-	director VARCHAR(2) NULL,
-	CONSTRAINT pk_docente PRIMARY KEY(id)
-)ENGINE=InnoDb;
-
 CREATE TABLE administrativo(
-	id INT(4) AUTO_INCREMENT NOT NULL,
+	id_admin INT(4) AUTO_INCREMENT NOT NULL,
 	nombre_a VARCHAR(20) NOT NULL,
 	apellidos_a VARCHAR(20) NOT NULL,
 	fecha_nacimiento_a DATE NOT NULL,
@@ -158,8 +29,156 @@ CREATE TABLE administrativo(
 	CONSTRAINT pk_administrativo PRIMARY KEY(id)
 )ENGINE=InnoDb;
 
+CREATE TABLE grado(
+		id INT(4) AUTO_INCREMENT NOT NULL,
+		id_admin_grado INT(4) NOT NULL,
+		nombre_g VARCHAR(3) NOT NULL,
+		CONSTRAINT pk_grado PRIMARY KEY(id),
+		CONSTRAINT fk_creador_grado FOREIGN KEY (id_admin_grado) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
+CREATE TABLE areas(
+	id_area INT(3) AUTO_INCREMENT NOT NULL,
+	nombre_area VARCHAR (60) NOT NULL,
+	color VARCHAR(10) NOT NULL,
+	CONSTRAINT pk_area PRIMARY KEY(id_area) ON DELETE CASCADE
+)ENGINE=InnoDb;
+
+CREATE TABLE materias_base(
+	id_base INT(4) AUTO_INCREMENT NOT NULL,
+	id_area_m INT(4) NOT NULL,
+	nombre_materia VARCHAR(50) NOT NULL,
+	porcentaje_materia_b INT(3) NOT NULL,
+	icono VARCHAR(60) NOT NULL,
+	CONSTRAINT pk_materias_base PRIMARY KEY (id_base),
+	CONSTRAINT fk_base_area FOREIGN KEY (id_area_m) REFERENCES areas(id_area) ON DELETE CASCADE
+)ENGINE=InnoDb;
+
+CREATE TABLE materia(
+		id INT(4) AUTO_INCREMENT NOT NULL,
+		id_grado_mat INT(4) NOT NULL,
+		id_materia_area INT(4) NOT NULL,
+		id_admin_materia INT(4) NOT NULL,
+		nombre_mat VARCHAR(30) NOT NULL,
+		indicadores_mat TEXT NOT NULL,
+		porcentaje_materia INT(3) NOT NULL,
+		icono VARCHAR(30)  NOT NULL,
+		asignacion VARCHAR(2) NOT NULL,
+		CONSTRAINT pk_materias PRIMARY KEY(id),
+		CONSTRAINT fk_grado_materia FOREIGN KEY(id_grado_mat) REFERENCES grado(id) ON DELETE CASCADE,
+		CONSTRAINT fk_materia_area FOREIGN KEY (id_materia_area) REFERENCES areas (id_area) ON DELETE CASCADE,
+		CONSTRAINT fk_admin_materia FOREIGN KEY (id_admin_materia) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
+CREATE TABLE periodo(
+	id INT(4) NOT NULL,
+	id_admin_periodo INT(4) NOT NULL,
+	nombre_periodo VARCHAR(10) NOT NULL,
+	fecha_inicio DATE NOT NULL,
+	fecha_fin DATE NOT NULL,
+	CONSTRAINT pk_periodo PRIMARY KEY(id),
+	CONSTRAINT fk_admin_periodo FOREIGN KEY (id_admin_periodo) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
+INSERT INTO periodo VALUES (1, '2022-01-01', '2022-02-02');
+INSERT INTO periodo VALUES (2, '2022-03-03', '2022-04-04');
+INSERT INTO periodo VALUES (3, '2022-05-05', '2022-06-06');
+-- -----------------------------------------------------
+
+CREATE TABLE padres(
+	id INT(4) AUTO_INCREMENT NOT NULL,
+	id_admin_padres INT(4) NOT NULL,
+	nombre_m VARCHAR(25) NULL,
+	apellidos_m VARCHAR(30) NULL,
+	fecha_nacimiento_m DATE NULL,
+	edad_m INT(2) NULL,
+	tipo_identificacion_m VARCHAR(11) NULL,
+	numero_m INT(12) NULL,
+	lugar_expedicion_m VARCHAR(50) NULL, 
+	fecha_expedicion_m DATE NULL,
+	telefono_m BIGINT(10) NULL,
+	ocupacion_m VARCHAR(50) NULL,
+	nombre_p VARCHAR(25) NULL,
+	apellidos_p VARCHAR(30) NULL,
+	fecha_nacimiento_p DATE NULL,
+	edad_p INT(2) NULL,
+	tipo_identificacion_p VARCHAR(11) NULL,
+	numero_p INT(12) NULL,
+	lugar_expedicion_p VARCHAR(50) NULL, 
+	fecha_expedicion_p DATE NULL,
+	telefono_p BIGINT(10) NULL,
+	ocupacion_p VARCHAR(50) NULL,
+	direccion VARCHAR(30) NOT NULL,
+	correo VARCHAR(30) NOT NULL, 
+	CONSTRAINT pk_padres PRIMARY KEY(id),
+	CONSTRAINT fk_admin_padres FOREIGN KEY (id_admin_padres) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
+CREATE TABLE estudiante(
+	id INT(4) AUTO_INCREMENT NOT NULL,
+	id_gradoE INT(4) NOT NULL,
+	id_familia_e INT(4) NOT NULL,
+	id_admin_estudiante INT(4) NOT NULL,
+	nombre_e VARCHAR(20) NOT NULL,
+	pellidos_e VARCHAR(20) NOT NULL,
+	fecha_nacimiento_e  DATE NOT NULL,
+	edad_e 	INT(2) NOT NULL,
+	sexo_e VARCHAR(10) NOT NULL,
+	tipo_identificacion_e VARCHAR(11)  NOT NULL,
+	numero_e INT(11) NOT NULL,
+	lugar_expedicion_e VARCHAR(50) NOT NULL,
+	fecha_expedicion_e DATE NOT NULL,
+	direccion_e VARCHAR(30) NOT NULL,
+	telefono_e BIGINT(10) NULL,
+	correo_e VARCHAR(50) NULL,
+	religion_e VARCHAR(20) NOT NULL,
+	incapacidad_medica_e VARCHAR(255) NOT NULL,
+	grupo_sanguineo_e VARCHAR(2) NOT NULL, 
+	rh_e VARCHAR(1) NOT NULL,
+	transporte VARCHAR(2) NOT NULL,
+	pae VARCHAR(2) NOT NULL,
+	img 	VARCHAR(255) NULL,
+	CONSTRAINT pk_estudiante PRIMARY KEY(id),
+	CONSTRAINT fk_grado_estudiante FOREIGN KEY (id_gradoE) REFERENCES grado (id),
+	CONSTRAINT fk_padres_estudiante FOREIGN KEY (id_familia_e) REFERENCES padres(id) ON DELETE CASCADE,
+	CONSTRAINT fk_admin_estudiante FOREIGN KEY (id_admin_estudiante) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
+CREATE TABLE docente(
+	id INT(4) AUTO_INCREMENT NOT NULL,
+	id_admin_docente INT(4) NOT NULL,
+	nombre_d VARCHAR(20) NOT NULL,
+	apellidos_d VARCHAR(20) NOT NULL,
+	fecha_nacimiento_d DATE NOT NULL,
+	edad_d INT(2) NOT NULL,
+	sexo_d VARCHAR(10) NOT NULL,
+	tipo_identificacion_d VARCHAR(10) NOT NULL,
+	numero_d INT(10) NOT NULL,
+	lugar_expedicion_d VARCHAR(50) NOT NULL,
+	fecha_expedicion_d DATE NOT NULL,
+	direccion_d VARCHAR(30) NOT NULL,
+	telefono_d BIGINT(10) NOT NULL,
+	correo_d VARCHAR(50) NOT NULL,
+	religion_d VARCHAR(20) NOT NULL,
+	incapacidad_medica_d VARCHAR(255) NOT NULL,
+	grupo_sanguineo_d VARCHAR(2) NOT NULL,
+	rh_d VARCHAR(1) NOT NULL,
+	fecha_posesion_d DATE NOT NULL,
+	numero_acta_posesion_d BIGINT NOT NULL,
+	numero_resolucion_posesion_d BIGINT NOT NULL,
+	pregrado_d VARCHAR(2) NOT NULL,
+	nombre_pregrado_d VARCHAR(30) NOT NULL,
+	posgrado_d VARCHAR(2) NOT NULL,
+	nombre_posgrado_d VARCHAR(30) NOT NULL,
+	img 	VARCHAR(255) NULL,
+	director VARCHAR(2) NULL,
+	CONSTRAINT pk_docente PRIMARY KEY(id),
+	CONSTRAINT fk_admin_docente FOREIGN KEY (id_admin_docente) REFERENCES administrativo (id_admin)
+)ENGINE=InnoDb;
+
 CREATE TABLE personal(
 	id INT(4) AUTO_INCREMENT NOT NULL,
+	id_admin_personal INT(4) NOT NULL,
 	nombre_per VARCHAR(20) NOT NULL,
 	apellidos_per VARCHAR(20) NOT NULL,
 	fecha_nacimiento_per DATE NOT NULL,
@@ -184,13 +203,15 @@ CREATE TABLE personal(
 	nombre_pregrado_per VARCHAR(30) NOT NULL,
 	posgrado_per VARCHAR(2) NOT NULL,
 	nombre_posgrado_per VARCHAR(30) NOT NULL,
-	CONSTRAINT pk_administrativo PRIMARY KEY(id)
+	CONSTRAINT pk_administrativo PRIMARY KEY(id),
+	CONSTRAINT fk_admin_personal FOREIGN KEY (id_admin_personal) REFERENCES administrativo (id_admin)
 )ENGINE=InnoDb;
 
 
 
 CREATE TABLE credenciales(
 	idC INT(4) AUTO_INCREMENT NOT NULL,
+	id_admin_credenciales INT(4) NOT NULL,
 	id_administrativo INT(4),
 	id_estudiante INT(4),
 	id_docente INT(4),
@@ -201,7 +222,8 @@ CREATE TABLE credenciales(
 	CONSTRAINT pk_credenciales PRIMARY KEY(id),
 	CONSTRAINT fk_usuario_credencial FOREIGN KEY(id_administrativo) REFERENCES administrativo(id) ON DELETE CASCADE,
 	CONSTRAINT fk_estudiante_credencial FOREIGN KEY(id_estudiante) REFERENCES estudiante(id) ON DELETE CASCADE,
-	CONSTRAINT fk_docente_credencial FOREIGN KEY(id_docente) REFERENCES docente(id) ON DELETE CASCADE
+	CONSTRAINT fk_docente_credencial FOREIGN KEY(id_docente) REFERENCES docente(id) ON DELETE CASCADE,
+	CONSTRAINT fk_admin_credenciales FOREIGN KEY (id_admin_credenciales) REFERENCES administrativo (id_admin)
 )ENGINE=InnoDb;
 
 
@@ -215,6 +237,7 @@ CREATE TABLE estudianteMateria(
 
 CREATE TABLE notas(
 id INT(4) AUTO_INCREMENT NOT NULL,
+id_admin_nota INT(4) NOT NULL,
 id_materia_n INT(4) NOT NULL,
 id_estudiante_n INT(4) NOT NULL,
 id_periodo_n INT(4) NOT NULL,
@@ -224,7 +247,8 @@ porcentejes INT(4) NOT NULL,
 CONSTRAINT pk_nota PRIMARY KEY(id),
 CONSTRAINT fk_materia_nota FOREIGN KEY(id_materia_n) REFERENCES materia(id) ON DELETE CASCADE,
 CONSTRAINT fk_estudiante_nota FOREIGN KEY (id_estudiante_n) REFERENCES estudiante(id) ON DELETE CASCADE,
-CONSTRAINT fk_periodo_nota FOREIGN KEY (id_periodo_n) REFERENCES periodo(id) ON DELETE CASCADE
+CONSTRAINT fk_periodo_nota FOREIGN KEY (id_periodo_n) REFERENCES periodo(id) ON DELETE CASCADE,
+CONSTRAINT fk_admin_notas FOREIGN KEY (id_admin_nota) REFERENCES administrativo (id_admin)
 )ENGINE=InnoDb;
 
 CREATE TABLE falla(
@@ -261,8 +285,8 @@ CREATE TABLE calendario(
 CREATE TABLE docenteMateria(
 	id_docente_mat INT(4) NOT NULL,
 	id_materia_doc INT(4) NOT NULL,
-	CONSTRAINT fk_materia_docente FOREIGN KEY(id_materia_doc) REFERENCES materia(id) ON DELETE CASCADE,
-	CONSTRAINT fk_doncente_materia FOREIGN KEY(id_docente_mat) REFERENCES docente(id) ON DELETE CASCADE
+	CONSTRAINT fk_materia_docente FOREIGN KEY (id_materia_doc) REFERENCES materia (id) ON DELETE CASCADE,
+	CONSTRAINT fk_doncente_materia FOREIGN KEY (id_docente_mat) REFERENCES docente (id) ON DELETE CASCADE
 )ENGINE=InnoDb;
 
 CREATE TABLE lunes(
@@ -374,13 +398,6 @@ CREATE TABLE director(
 	CONSTRAINT fk_grado_dir_docente FOREIGN KEY(id_grado_dir) REFERENCES grado(id) ON DELETE CASCADE
 )ENGINE=InnoDb;
 
-CREATE TABLE areas(
-	id_area INT(3) AUTO_INCREMENT NOT NULL,
-	nombre_area VARCHAR (60) NOT NULL,
-	color VARCHAR(10) NOT NULL,
-	CONSTRAINT pk_area PRIMARY KEY(id_area) ON DELETE CASCADE
-)ENGINE=InnoDb;
-
 CREATE TABLE aulaGrado(
 aula_grado INT(3) AUTO_INCREMENT NOT NULL,
 id_aula_grado INT(3) NOT NULL,
@@ -393,6 +410,7 @@ CONSTRAINT fk_grado_aula FOREIGN KEY (id_grado_aula) REFERENCES grado(id) ON DEL
 -- ----------------COGNITIVO---------------------------------------------
 CREATE TABLE cognitivo(
 id_cognitivo INT(3) AUTO_INCREMENT NOT NULL,
+
 porcentaje_cognitivo INT(3) DEFAULT 0,
 porcentaje_evaluacion INT(3) DEFAULT 0,
 porcentaje_trimestral INT(3) DEFAULT 0,
@@ -407,8 +425,8 @@ id_periodo_e INT(3) NOT NULL,
 id_cognitivo_e INT(3) NOT NULL,
 nota_evaliacion INT(2) NOT NULL,
 CONSTRAINT pk_evaluaciones PRIMARY KEY (id_evaluacion),
-CONSTRAINT fk_estudiante_evaluacion FOREIGN KEY (id_estudiante_e) REFERENCES estudiante(id),
-CONSTRAINT	 fk_mateia_evaluacion FOREIGN KEY (id_materia_e) REFERENCES materia (id),
+CONSTRAINT fk_estudiante_evaluacion FOREIGN KEY (id_estudiante_e) REFERENCES estudiante(id) ON DELETE CASCADE,
+CONSTRAINT	 fk_mateia_evaluacion FOREIGN KEY (id_materia_e) REFERENCES materia (id) ON DELETE CASCADE,
 CONSTRAINT fk_periodo_evaluacion FOREIGN KEY (id_periodo_e) REFERENCES periodo (id),
 CONSTRAINT fk_cognitivo_evaluacion FOREIGN KEY (id_cognitivo_e) REFERENCES cognitivo (id_cognitivo)
 )ENGINE=InnoDb;
@@ -421,8 +439,8 @@ id_periodo_t INT(3) NOT NULL,
 id_cognitivo_t INT(3) NOT NULL,
 nota_trimestral INT(2) NOT NULL,
 CONSTRAINT pk_trimestral PRIMARY KEY (id_trimestral),
-CONSTRAINT fk_estudiante_trimestal FOREIGN KEY (id_estudiante_t) REFERENCES estudiante(id),
-CONSTRAINT	 fk_mateia_trimestral FOREIGN KEY (id_materia_t) REFERENCES materia (id),
+CONSTRAINT fk_estudiante_trimestal FOREIGN KEY (id_estudiante_t) REFERENCES estudiante(id) ON DELETE CASCADE,
+CONSTRAINT	 fk_mateia_trimestral FOREIGN KEY (id_materia_t) REFERENCES materia (id) ON DELETE CASCADE,
 CONSTRAINT fk_periodo_trimestral FOREIGN KEY (id_periodo_t) REFERENCES periodo (id),
 CONSTRAINT fk_cognitivo_trimestral FOREIGN KEY (id_cognitivo_t) REFERENCES cognitivo (id_cognitivo)
 )ENGINE=InnoDb;
@@ -481,8 +499,8 @@ id_periodo_apreciativa INT(3) NOT NULL,
 id_actitudinal_apreciativa INT(3) NOT NULL,
 nota_apreciativa INT(2) NOT NULL,
 CONSTRAINT pk_apreciativa PRIMARY KEY (id_apreciativa),
-CONSTRAINT fk_estudiante_apreciativa FOREIGN KEY (id_estudiante_apreciativa) REFERENCES estudiante(id),
-CONSTRAINT	 fk_mateia_apreciativa FOREIGN KEY (id_materia_apreciativa) REFERENCES materia (id),
+CONSTRAINT fk_estudiante_apreciativa FOREIGN KEY (id_estudiante_apreciativa) REFERENCES estudiante(id) ON DELETE CASCADE,
+CONSTRAINT	 fk_mateia_apreciativa FOREIGN KEY (id_materia_apreciativa) REFERENCES materia (id) ON DELETE CASCADE,
 CONSTRAINT fk_periodo_apreciativa FOREIGN KEY (id_periodo_apreciativa) REFERENCES periodo (id),
 CONSTRAINT fk_cognitivo_apreciativa FOREIGN KEY (id_actitudinal_apreciativa) REFERENCES actitudinal (id_actitudinal)
 )ENGINE=InnoDb;
@@ -495,8 +513,8 @@ id_periodo_autoevaluacion INT(3) NOT NULL,
 id_actitudinal_autoevaluacion INT(3) NOT NULL,
 nota_trimestral INT(2) NOT NULL,
 CONSTRAINT pk_autoevaluacion PRIMARY KEY (id_autoevaluacion),
-CONSTRAINT fk_estudianteautoevaluacion FOREIGN KEY (id_estudiante_autoevaluacion) REFERENCES estudiante(id),
-CONSTRAINT	 fk_mateia_autoevaluacion FOREIGN KEY (id_materia_autoevaluacion) REFERENCES materia (id),
+CONSTRAINT fk_estudianteautoevaluacion FOREIGN KEY (id_estudiante_autoevaluacion) REFERENCES estudiante(id) ON DELETE CASCADE,
+CONSTRAINT	 fk_mateia_autoevaluacion FOREIGN KEY (id_materia_autoevaluacion) REFERENCES materia (id) ON DELETE CASCADE,
 CONSTRAINT fk_periodo_autoevaluacion FOREIGN KEY (id_periodo_autoevaluacion) REFERENCES periodo (id),
 CONSTRAINT fk_actitudinal_autoevaluacion FOREIGN KEY (id_actitudinal_autoevaluacion) REFERENCES actitudinal (id_actitudinal)
 )ENGINE=InnoDb;
@@ -572,12 +590,13 @@ CREATE TABLE promedioEstudiante(
 	CONSTRAINT pk_promedio PRIMARY KEY (id_avg),
 	CONSTRAINT fk_estudiante_avg FOREIGN KEY (id_estudiante_avg) REFERENCES estudiante (id) ON DELETE CASCADE,
 	CONSTRAINT fk_periodo_avg FOREIGN KEY (id_periodo_avg) REFERENCES periodo (id),
-	CONSTRAINT fk_grado_avg FOREIGN KEY (id_grado_avg) REFERENCES grado (id) ON DELETE CASCADE
+	CONSTRAINT fk_grado_avg FOREIGN KEY (id_grado_avg) REFERENCES grado (id)
 )ENGINE=InnoDb;
 
 -- -----------------------------------------------BOLETIN ----------------------------------
 CREATE TABLE boletin(
 	id_boletin  INT(10) AUTO_INCREMENT NOT NULL,
+	id_docente_boletin INT(4) NOT NULL,
 	id_estudiante_boletin INT(10) NOT NULL,
 	id_materia_boletin INT(10) NOT NULL,
 	id_area_boletin INT(10) NOT NULL,
@@ -596,15 +615,19 @@ CREATE TABLE boletin(
 	CONSTRAINT fk_estudiante_boletin FOREIGN KEY (id_estudiante_boletin) REFERENCES estudiante (id) ON DELETE CASCADE,
 	CONSTRAINT fk_materia_boletin FOREIGN KEY (id_materia_boletin) REFERENCES materia (id) ON DELETE CASCADE,
 	CONSTRAINT fk_area_boletin FOREIGN KEY (id_area_boletin) REFERENCES areas (id_area),
-	CONSTRAINT fk_periodo_boletin FOREIGN KEY (id_periodo_boletin) REFERENCES periodo (id)
+	CONSTRAINT fk_periodo_boletin FOREIGN KEY (id_periodo_boletin) REFERENCES periodo (id),
+	CONSTRAINT fk_boletin_docente FOREIGN KEY (id_docente_boletin) REFERENCES administrativo (id_admin)
 )ENGINE=InnoDb;
 
 # tabla para avilitar o desabilitar el envio de notas al boletin general
 CREATE TABLE habilitarBoletin(
 	id_estado INT(3) NOT NULL,
+	id_admin_boletin_habil INT NOT NULL,
 	estado VARCHAR(15) NOT NULL,
 	CONSTRAINT pd_habilitar_boletin PRIMARY KEY (id_estado)
+	CONSTRAINT fk_admin_habil_boletin FOREIGN KEY (id_admin_boletin_habil) REFERENCES administrativo (id_admin)
 )ENGINE=InnoDb;
+
 INSERT INTO habilitarBoletin VALUES(1, 'Deshabilitado');
 -- ----------------------------------------------------------------------------
 
@@ -620,7 +643,7 @@ CREATE TABLE puestos(
 	CONSTRAINT pk_pruesto PRIMARY KEY (id_puesto),
 	CONSTRAINT fk_estudiante_puesto FOREIGN KEY (id_estudiante_puesto) REFERENCES estudiante (id) ON DELETE CASCADE,
 	CONSTRAINT fk_periodo_puesto FOREIGN KEY (id_periodo_puesto) REFERENCES periodo (id),
-	CONSTRAINT fk_grado_puesto FOREIGN KEY (id_grado_puesto) REFERENCES grado (id) ON DELETE CASCADE
+	CONSTRAINT fk_grado_puesto FOREIGN KEY (id_grado_puesto) REFERENCES grado (id)
 )ENGINE=InnoDb;
 
 	# Tablero de actividades
